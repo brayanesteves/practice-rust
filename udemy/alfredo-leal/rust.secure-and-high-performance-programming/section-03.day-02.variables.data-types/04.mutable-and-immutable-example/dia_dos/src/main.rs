c5fbc48fb10098641125904d6875bbdc58c213d8
Brayan Esteves <brayan.esteves93@gmail.com>
2026-02-28 10:56:38 -0400

    [Udemy][Alfredo Leal] Section 03. Day 02. Variables Data Types  - 04. Mutable and Inmutable Example [28/02/2026]

---
+++ udemy/alfredo-leal/rust.secure-and-high-performance-programming/section-03.day-02.variables.data-types/04.mutable-and-immutable-example/dia_dos/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // println!("Hello, world!");
+
+    // Ejemplo de inmutabilidad.
+    // let y = 1; // Inmutable (No se puede reasignar).
+    // y = 2;
+    // println!("El valor de 'y' es: {y}");
+
+    // Ejemplo mutable.
+    let mut x = 3; // Mutable (Se puede reasignar).
+    println!("El valor de 'x' es: {x}");
+    x = 6;
+    println!("El valor de 'x' es: {x}");
+}
