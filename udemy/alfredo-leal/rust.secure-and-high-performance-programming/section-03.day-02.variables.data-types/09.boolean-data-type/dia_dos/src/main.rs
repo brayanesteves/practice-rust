03e1f970ac432c4bb4c6e0d77328840e5d7821dc
Brayan Esteves <brayan.esteves93@gmail.com>
2026-03-01 14:44:46 -0400

    [Udemy][Alfredo Leal] Section 03. Day 02. Variables Data Types  - 09. Boolean Data Type [01/03/2026]

---
+++ udemy/alfredo-leal/rust.secure-and-high-performance-programming/section-03.day-02.variables.data-types/09.boolean-data-type/dia_dos/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    /**
+     * [ENGLISH]
+     * 
+     * ==========
+     * 
+     * [SPANISH]
+     * 
+     * Shadowing.
+     * Booleanos.
+     */
+    let t = true;
+    let f: bool = false; // Explicito, el tipo booleano.
+}
