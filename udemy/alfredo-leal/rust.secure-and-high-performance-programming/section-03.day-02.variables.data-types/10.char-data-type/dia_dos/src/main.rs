37702d6763c5ab35e90eb6c2e91581f1bad37601
Brayan Esteves <brayan.esteves93@gmail.com>
2026-03-01 14:48:26 -0400

    [Udemy][Alfredo Leal] Section 03. Day 02. Variables Data Types  - 10. Char Data Type [01/03/2026]

---
+++ udemy/alfredo-leal/rust.secure-and-high-performance-programming/section-03.day-02.variables.data-types/10.char-data-type/dia_dos/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    /**
+     * [ENGLISH]
+     * 
+     * ==========
+     * 
+     * [SPANISH]
+     * 
+     * Shadowing.
+     * Char.
+     */
+    let c1 = 'c';
+    let c2: char = 'C'; // Explicito, el tipo booleano.
+    let cat = '😻';
+}
