4bf119bcbba1311edda91109d7051e215a5d8931
Brayan Esteves <brayan.esteves93@gmail.com>
2026-03-01 14:39:51 -0400

    [Udemy][Alfredo Leal] Section 03. Day 02. Variables Data Types  - 08. Float Data Type [01/03/2026]

---
+++ udemy/alfredo-leal/rust.secure-and-high-performance-programming/section-03.day-02.variables.data-types/08.float-data-type/dia_dos/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    /**
+     * [ENGLISH]
+     * 
+     * ==========
+     * 
+     * [SPANISH]
+     * 
+     * Shadowing.
+     * Flotante
+     */
+    let a = 3.0; // f64
+    let b: f32 = 4.0; // f32
+}
