dcd1df13ab9adb77ce24385bf7cff4046463e16d
Brayan Esteves <brayan.esteves93@gmail.com>
2026-03-14 11:24:38 -0400

    [Udemy][Alfredo Leal] Section 05. Day 04. Ownership Borrowing Slice Type Concepts - 07. Scope and Assignment [14/03/2026]

---
+++ udemy/alfredo-leal/rust.secure-and-high-performance-programming/section-05.days-04.ownership-borrowing-slice-type-concepts/07.scope-and-assignment/dia_cuatro/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    /**
+     * [ENGLISH]
+     * 
+     * ==========
+     * 
+     * [SPANISH]
+     * Alcance y asignación.
+     */
+    let mut s = String::from("hello");
+    s = String::from("ahoy");
+
+    println!("{s} world!");
+}
