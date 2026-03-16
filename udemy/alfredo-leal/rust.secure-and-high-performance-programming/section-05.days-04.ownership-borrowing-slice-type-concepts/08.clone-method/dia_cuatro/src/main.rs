c056f125e8c48035aea5597ebe5b665d72fc6f3a
Brayan Esteves <brayan.esteves93@gmail.com>
2026-03-16 15:01:05 -0400

    [Udemy][Alfredo Leal] Section 05. Day 04. Ownership Borrowing Slice Type Concepts - 08. Clone Method [16/03/2026]

---
+++ udemy/alfredo-leal/rust.secure-and-high-performance-programming/section-05.days-04.ownership-borrowing-slice-type-concepts/08.clone-method/dia_cuatro/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    /**
+     * [ENGLISH]
+     * 
+     * ==========
+     * 
+     * [SPANISH]
+     * Método 'clone'.
+     */
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {s1}, s2 = {s2}");
+}
